1f27cd6aa97ad11e9c1940c4e8c53ed630249651
Talha Rafi <talhar9@gmail.com>
2023-11-27 22:32:54 +0000

    adding todo's

--- main.go
+++ main.go
@@ -10,11 +10,18 @@ func main() {
 
 	router := gin.Default()
 
+	//TODO make getPrayerTimes return the whole month
+	// use a cron job to run get prayertimes
+	// use the same cron job to add the prayer times to a redis in memory database for that month
+	// that way the data will persist if the application goes down
+	// add v1 GET api to make a call, given a date, to recieve all prayer times for that day, from the redis server
+
 	location := "Europe/London"
 	pt, err := GetPrayerTimes(location)
 	if err != nil {
 		fmt.Errorf("error executing GetPrayerTimes, err %w", err)
 	}
+
 	// store it in the global variable to be accessed outside
 	fmt.Println(pt.Isha)
 	fmt.Println(pt.PrayerDate)
