ff7a6fa48f90f16474058a79fef1354479a2718c
Talha Rafi <talhar9@gmail.com>
2024-02-11 12:00:57 +0000

    adding api call functiomnality for frontend for all stats

---
+++ frontend/tpm-frontend/src/pages/api/getAllStats.ts
@@ -0,0 +1,44 @@
+import { parseCookies } from "nookies";
+
+interface RequestObject {
+  query: {
+    date: string;
+  };
+  headers: {
+    cookie?: string;
+  };
+}
+import { NextApiResponse } from "next";
+
+export default async function getAllStats(
+  req: RequestObject,
+  res: NextApiResponse
+) {
+  const { date } = req.query;
+  const cookies = parseCookies({ req });
+
+  // Extract the jwt cookie
+  const jwtCookie = cookies.jwt;
+
+  const response = await fetch(
+    `http://localhost:8080/api/v1/restricted/getAllStats}`,
+    {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        // Include jwt cookie in the headers of the outgoing request
+        Authorization: jwtCookie,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  console.log(typeof data);
+  console.log(data);
+  res.status(200).json(data);
+  return data;
+}
