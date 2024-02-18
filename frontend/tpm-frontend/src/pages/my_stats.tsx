7a76a0285b62d5359712268b7b6a05a3fb83d158
Talha Rafi <talhar9@gmail.com>
2024-02-18 13:49:05 +0000

    calling pie chart compoennt into my stats page

--- frontend/tpm-frontend/src/pages/my_stats.tsx
+++ frontend/tpm-frontend/src/pages/my_stats.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Roboto_Mono, Bebas_Neue } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { Inter } from "next/font/google";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import React from "react";
 import { Pie } from "react-chartjs-2";
@@ -19,6 +19,7 @@ import calculateTimeTillRefresh from "@/functions/calculateTimeTillRefresh";
 import NavbarComponent from "@/components/NavBar";
 import { Row, Col } from "react-bootstrap";
 import getAllStats from "@/functions/getAllStats";
+import PieChartProductiveVal from "@/components/PieChartProductiveVal";
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
@@ -28,60 +29,16 @@ ChartJS.register(ArcElement, Tooltip, Legend, Title);
 const inter = Inter({ subsets: ["latin"] });
 // const [allStats, setAllStats] = useState<string | null>(null);
 
-const MyPieChart = () => {
-  const data = {
-    labels: ["Productive", "Unproductive"],
-    datasets: [
-      {
-        label: "My First Dataset",
-        data: [12, 19],
-        backgroundColor: ["#36A2EB", "#FF6384"], // Use custom colors
-        hoverOffset: 4,
-        borderWidth: 1, // Add border width
-        borderColor: "#fff", // Add border color
-      },
-    ],
-  };
-
-  const options = {
-    plugins: {
-      title: {
-        display: true,
-        // text: "Your total",
-        font: {
-          size: 16,
-        },
-      },
-      legend: {
-        display: true,
-        position: "bottom",
-      },
-      tooltip: {
-        enabled: true,
-      },
-    },
-    animation: {
-      animateRotate: true, // Enable rotation animation
-      animateScale: true, // Enable scaling animation
-    },
-    rotation: -0.5 * Math.PI, // Rotate the chart to a specific angle
-    responsive: true, // Make the chart responsive
-  };
-
-  return (
-    <div>
-      <h2>Todays productivity</h2>
-      <Pie data={data} options={options} />
-    </div>
-  );
-};
-
 export default function myStats() {
+  const [dailyStats, setDailyStats] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
-      const myStats = await getAllStats();
+      var myStats = await getAllStats();
 
       if (myStats) {
+        myStats = JSON.parse(myStats);
+        myStats = setDailyStats(myStats);
         console.log(myStats);
       } else {
         console.log("no stats from api");
@@ -100,7 +57,7 @@ export default function myStats() {
         />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <MyPieChart></MyPieChart>
+        <PieChartProductiveVal dailyStats={dailyStats} />
         <div>
           <h1>my stats </h1>
         </div>
